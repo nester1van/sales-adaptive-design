4098942b356f4988a42b2970864ee001d06cac5d
Ivan Nesterov <61783537+nester1van@users.noreply.github.com>
2020-07-11 16:04:11 +0400

    Update changeCategory.js

--- js/changeCategory.js
+++ js/changeCategory.js
@@ -1,15 +1,20 @@
 (function(){
   const menuLinks = document.querySelectorAll(".menu-item__link");
   const headerTitle = document.querySelector(".header__title");
-    // menuLink.classList.remove("menu-item__link_active")
   for (let menuLink of menuLinks) {
-    menuLink.addEventListener("click", (e) => {
-      menuLink.classList.toggle("menu-item__link_active");
-      if (!menuLink.firstElementChild.src.includes("_")) {
-        menuLink.firstElementChild.src = menuLink.firstElementChild.src.replace(".svg", "");
-        menuLink.firstElementChild.src += "_a.svg";
+        menuLink.addEventListener("click", (e) => {
+          for (let menuLinkActive of menuLinks) {
+            if (menuLinkActive.classList.contains("menu-item__link_active")) {
+              menuLinkActive.classList.remove("menu-item__link_active");
+              menuLinkActive.firstElementChild.src = 
+                menuLinkActive.firstElementChild.src.replace("_a.svg", ".svg");
+            }
+          }
+          menuLink.classList.toggle("menu-item__link_active");
+          menuLink.firstElementChild.src = 
+            menuLink.firstElementChild.src.replace(".svg", "_a.svg");
+          headerTitle.innerHTML = menuLink.lastElementChild.textContent;
+
+        });
       }
-      headerTitle.innerHTML = menuLink.lastElementChild.textContent;
-    });
-  }
 })();
